efec8a0ccf6733760f405fd0c7151ac9cee296a9
CSUSER <csuser@csmacbook.contatan.local>
2018-02-16 10:43:18 +0530

    simple test

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,10 +1,3 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
 describe('Release test',()=>{
   it('Always true statement',()=>{
     expect(true).toBe(true)
